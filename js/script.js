5c7cb1479cd1cfe476809699a3c6f59aff470623
aldimaulana14 <aldimaulana14102004@gmail.com>
2024-12-30 08:47:39 +0700

    mengubah animasi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("scroll", function () {
     const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
+    if (window.scrollY > 0) {
       navbar.classList.add("scrolled");
       navbar.classList.remove("transparent");
     } else {
